dbf958b2564ac40194213d33206695a5c6280602
3454535 <ayansh.chaudhuri@gmail.com>
2026-08-24 15:53:05 -0500

    Update pxt.json, custom.ts

---
+++ custom.ts
@@ -0,0 +1,21 @@
+
+/**
+* Use this file to define custom functions and blocks.
+* Read more at https://makecode.microbit.org/blocks/custom
+*/
+
+
+
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#0fbc11 icon=""
+namespace robot {
+    //% block
+    export function Trace(robot_x: number,robot_y: number,robot_move:number):void {
+        while(robot_x<4); {
+        led.toggle(robot_x, robot_y)
+        robot_x+1;
+        }
+    }
+}
